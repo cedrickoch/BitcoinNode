632affe624e3db9c3689c4582f7be99a4c1f2e51
cedrickoch <cedrickoch@Mac.lan>
2025-05-08 12:00:13 +0200

    Changed to lncm bitoin image

--- import.sh
+++ import.sh
@@ -1,16 +1,16 @@
-#/bin/sh
+#!/bin/sh
 
-LOGFILE=/home/bitcoin/.bitcoin/debug.log
+LOGFILE=/data/.bitcoin/debug.log
 SEARCH_TEXT="00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
 
 echo "Import starting"
-bitcoind -daemon -printtoconsole -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -txindex=1 -datadir=/home/bitcoin/.bitcoin
+bitcoind -daemon -printtoconsole -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -txindex=1 -datadir=/data/.bitcoin
 
 # Wait for the block headers to be downloaded
 while true; do
     # Check if the text appears in the log file
     if grep -q "$SEARCH_TEXT" "$LOGFILE"; then
-        echo "Text found: '$SEARCH_TEXT'"
+        echo "Block headers downloaded"
         break
     fi
     
@@ -20,5 +20,5 @@ while true; do
 done
 
 echo "Block headers downloaded, starting import"
-bitcoin-cli -datadir=/home/bitcoin/.bitcoin loadtxoutset /utxo-snapshot-height-840000.dat
+bitcoin-cli -datadir=/data/.bitcoin loadtxoutset /utxo-snapshot-height-840000.dat
 echo "Import completed"
